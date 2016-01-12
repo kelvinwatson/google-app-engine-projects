7053e1c007bbac8a36ebf86111adafd65d8fbe6b
Kelvin Watson <watsokel@oregonstate.edu>
2016-01-12 13:49:11 -0800

    view page

--- malenah_lgbt_providers/admin.py
+++ malenah_lgbt_providers/admin.py
@@ -12,7 +12,7 @@ class AdminHandler(base.BaseHandler):
         self.template_values = {
             'title': "MALENAH Administrator Portal",
             'header_title': "Welcome to the M.A.L.E.N.A.H. Administrator Portal",
-            'last_accessed': datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+            'last_accessed': datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S'),
             }
 
     def get(self):
@@ -25,25 +25,19 @@ class AdminHandler(base.BaseHandler):
             console.log(k)
             provider = Entity.Provider(parent=k)
             console.log(provider)
-            provider.first_name = self.request.get('first-name')
-            provider.last_name = self.request.get('last-name')
+            provider.first_name = self.request.get('first_name')
+            provider.last_name = self.request.get('last_name')
             provider.phone = self.request.get('phone')
             provider.email = self.request.get('email')
             provider.website = self.request.get('website')
-
-            provider.best_time = datetime.strptime(self.request.get('best-time'), "%H:%M").time()
-            console.log(provider.best_time)
-
+            provider.best_time = datetime.strptime(self.request.get('best_time'), "%H:%M").time()
+            #console.log(provider.best_time)
             provider.designation = self.request.get('designation')
             provider.services = [ndb.Key(urlsafe=x) for x in self.request.get_all('services[]')]
-
-            console.log(self.request.get('accept-new-patients'))
+            #console.log(self.request.get('accept_new_patients'))
             provider.accept_new_patients = True if (self.request.get('accept-new-patients') == "True") else False
             console.log(provider.accept_new_patients)
-
-            #provider.put()
-            #console.log(first_name)
-            #console.log(last_name)
+            new_key = provider.put()
             self.template_values['post_result'] = 'Provider '+provider.first_name+' '+provider.last_name+' successfully added'
         elif action=='add_designation':
             designation = self.request.get('designation')
@@ -53,7 +47,7 @@ class AdminHandler(base.BaseHandler):
             self.template_values['post_result'] = service+' service successfully added'
         else:
             self.template_values['post_result'] = 'Unknown action'
-        self.render('admin.html',self.template_values)
+        self.redirect('/view?key='+ new_key.urlsafe()+ '&type=healthcare_provider')
 
 '''
         provider = {}
--- malenah_lgbt_providers/view.py
+++ malenah_lgbt_providers/view.py
@@ -1,6 +1,7 @@
 import base
 import log as console
 from datetime import datetime
+from google.appengine.ext import ndb
 
 class ViewHandler(base.BaseHandler):
     def __init__(self, request, response):
@@ -8,11 +9,36 @@ class ViewHandler(base.BaseHandler):
         console.log(datetime.now().time())
         self.template_values = {
             'title': "Record Added",
-            'header_title': "You added the following record:",
-            'last_accessed': datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+            'header_title': "Record Added",
+            'last_accessed': datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S'),
             }
 
     def get(self):
-        #get the key
-        #get the entity using key.get()
-        self.render('view.html', self.template_values) #call the overridden render (above)
+        t = {}
+        t['type'] = self.request.get('type')
+        console.log('type='+t['type'])
+        self.template_values['record_type'] = t
+        console.log(self.template_values['record_type'])
+        console.log(type(self.template_values['record_type']))
+        if t['type']=='healthcare_provider':
+            k = ndb.Key(urlsafe=self.request.get('key')) #get key string and construct key
+            e = k.get() #get the entity from the database associated with that key
+            #set the template values
+            t['name']='Healthcare Provider'
+            self.template_values['first_name'] = e.first_name
+            self.template_values['last_name'] = e.last_name
+            self.template_values['phone'] = e.phone
+            self.template_values['email'] = e.email
+            self.template_values['website'] = e.website
+            self.template_values['best_time'] = e.best_time
+            self.template_values['designation'] = e.designation
+            self.template_values['services'] = e.services #TODO: on view.html, just needs to display, not prepopulate with checks
+        elif t['type']=='designation':
+            t['name']='Designation'
+            console.log("view added designation")
+        elif t['type']=='services':
+            t['name']='Service(s)'
+            console.log("view added services")
+        else:
+            console.log("wrong type")
+        base.BaseHandler.render(self, 'view.html', self.template_values) #call the overridden render (above)
