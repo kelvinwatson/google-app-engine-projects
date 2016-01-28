2b9790d94f3277ac7291dc528ca4cb7ae27824e0
Kelvin Watson <watsokel@oregonstate.edu>
2016-01-27 21:04:45 -0800

    put reply

--- malenah-api/ReplyHandler.py
+++ malenah-api/ReplyHandler.py
@@ -9,14 +9,12 @@ class ReplyHandler(webapp2.RequestHandler):
         self.existing_providers = [{'first_name':qe.first_name,'last_name':qe.last_name,'designation':qe.designation,'organization':qe.organization,'specializations':qe.specializations,'phone':qe.phone,'email':qe.email,'website':qe.website,'accepting_new_patients':qe.accepting_new_patients,'key':qe.key.id()} for qe in E.Provider.query(ancestor=ndb.Key(E.Provider, self.app.config.get('M-P')))]
         self.existing_reviews = [{'username':qe.username,'rating':qe.rating,'comment':qe.comment,'replies':qe.replies,'provider':qe.provider.id(),'key':qe.key.id()} for qe in E.Review.query()]
         self.existing_replies = [{'username':qe.username,'comment':qe.comment,'review':qe.review.id(),'provider':qe.provider.id(),'key':qe.key.id()} for qe in E.Reply.query()]
-        print(self.existing_replies)
+        #print(self.existing_replies)
 
 
     def get(self, *args, **kwargs):
         self.response.headers['Content-Type'] = 'application/json'
-        print('Reply Handler\n')
-        print('args: '+str(args)+'\n')
-        print('kwargs: '+str(kwargs))
+        #print('GET Reply Handler\n')
 
         obj={}
         if not kwargs or kwargs is None: #GET /reply or /reply/
@@ -29,8 +27,6 @@ class ReplyHandler(webapp2.RequestHandler):
                         obj['status'] = self.response.status
                         self.response.write(json.dumps(obj))
         else: #GET /provider/pid or /provider/pid (print only the requested provider)
-            print(args)
-            print(kwargs)
             if 'repid' in kwargs:
                 print('there is a repid so only show that reply')
             elif 'revid' in kwargs:
@@ -58,10 +54,10 @@ class ReplyHandler(webapp2.RequestHandler):
              print(obj)
              self.expand_review(obj)
              self.expand_provider(obj) #for json output
-        # else:
-        #     self.response.clear()
-        #     self.response.set_status(400, status_message)
-        #     obj['status'] = self.response.status
+        else:
+             self.response.clear()
+             self.response.set_status(400, status_message)
+             obj['status'] = self.response.status
         self.response.write(json.dumps(obj))
         return
 
@@ -109,4 +105,47 @@ class ReplyHandler(webapp2.RequestHandler):
 
     #https://cloud.google.com/appengine/docs/python/tools/webapp/requesthandlerclass
     def put(self, *args, **kwargs):
-        
+        self.response.headers['Content-Type'] = 'application/json'
+        print(args)
+        print(kwargs)
+        obj={}
+        if not kwargs or kwargs is None or 'repid' not in kwargs: #GET /reply or /reply/
+            self.response.clear()
+            self.response.set_status(400, status_message)
+            obj['status'] = self.response.status
+        else: #reply id is in kwarg
+            match = next((er for er in self.existing_replies if er['key']==int(kwargs['repid'])), None) #
+            if match is not None:
+                print('do the mod')
+                properties = {
+                    'username': self.request.get('username'), #required
+                    'comment': self.request.get('comment'),
+                    'review': self.request.get('review'),
+                    'provider': self.request.get('provider_key'), #required (int id)
+                  }
+                print(properties)
+                status_message = self.validate_input(properties)
+                print(status_message)
+                obj={}
+                if 'Invalid' not in status_message: #check for empty or invalid fields
+                    #no parent key needed as each Review entity should be in its own entity group
+                    #construct key
+                    e = ndb.Key(E.Reply, int(kwargs['repid'])).get()
+                    e.populate(**properties)
+                    e.put()
+                    obj = e.to_dict()
+                    self.response.set_status(200, status_message)
+                    obj['status'] = self.response.status
+                    print(obj)
+                    self.expand_review(obj)
+                    self.expand_provider(obj) #for json output
+                else:
+                     self.response.clear()
+                     self.response.set_status(400, status_message)
+                     obj['status'] = self.response.status
+            else:
+                 self.response.clear()
+                 self.response.set_status(400, '- Invalid input. No match for reply id.')
+                 obj['status'] = self.response.status
+        self.response.write(json.dumps(obj))
+        return
